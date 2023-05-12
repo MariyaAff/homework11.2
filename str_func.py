7463333c0ed2de86b4d12e99b6164db62bc59aa3
Мария Юркшайтене <masha.afan@bk.ru>
2023-05-12 21:31:35 +0300

    add new func 2

---
+++ str_func.py
@@ -0,0 +1,12 @@
+"""Функция, возвращающая строку заглавными буквами"""
+
+
+def func(value: str):
+    return value.upper()
+
+
+"""Функция, возвращающая первые заглавные буквы строки"""
+
+
+def func_new(value_new: str):
+    return value_new.title()
